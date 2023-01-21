946c8e182cfa57ea70f404fcb6f20424ea99813f
Aleksei Starkov <phenman64@gmail.com>
2023-01-21 23:40:45 +0100

    Add files via upload

---
+++ test_maui_net7/test_maui_net7/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace test_maui_net7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ test_maui_net7/test_maui_net7/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace test_maui_net7;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
+
